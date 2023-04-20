d95a2c69fb620e8c53262ac3d525a14b716092d3
klin2391 <112966198+klin2391@users.noreply.github.com>
2023-04-20 11:38:23 -0400

    Update Graph

--- src/Grapher.java
+++ src/Grapher.java
@@ -1,11 +1,19 @@
-// https://stackoverflow.com/questions/47718958/java-swing-line-graph
+/*
+ * Grapher.java
+ * by Kevin Lin (lin2391@bu.edu)
+ * 20APR2023
+ * 
+ * This class used to create a graph of the stock's history
+ * Adapted from https://stackoverflow.com/questions/47718958/java-swing-line-graph
+ */
+
+
 import java.awt.*;
 import javax.swing.*;
 import java.util.ArrayList;
 
 public class Grapher extends JPanel{
-    // private int[] yCoords;
-    private ArrayList <Double> yCoords;
+    private ArrayList <Double> yCoords;         // The prices of the stock
     private int startX;
     private int startY;
     private int endX;
@@ -15,14 +23,12 @@ public class Grapher extends JPanel{
     private int prevX;
     private int prevY;
     private Double max;
-    private Double min;
     private boolean gain;
 
+    // Constructor
     public Grapher(ArrayList <Double> yCoords, Double profit) {
         this.yCoords = yCoords;
-        max = yCoords.stream().max(Double::compareTo).get();
-        System.out.println("max: " + max);
-        min = yCoords.stream().min(Double::compareTo).get();
+        max = yCoords.stream().max(Double::compareTo).get();        // We find the max value in the arraylist
         int maxInt = (int) (max / 10);
         max = (maxInt + 1) * 10.0;
         startX = 100;
@@ -41,31 +47,29 @@ public class Grapher extends JPanel{
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        //We draw in the following 2 loops the grid so it's visible what I explained before about each "unit"
         g2d.setColor(Color.BLACK);
-        for (int i = startX; i <= endX; i += unitX) {
+        for (int i = startX; i <= endX; i += unitX) {       // We draw the grid here
             g2d.drawLine(i, startY, i, endY);
         }
 
-        for (int i = startY; i <= endY; i += unitY) {
+        for (int i = startY; i <= endY; i += unitY) {       // We draw the grid here
             g2d.drawLine(startX, i, endX, i);
         }
 
-        //We draw the axis here instead of before because otherwise they would become blue colored.
+        
         g2d.setColor(Color.BLACK);
-        g2d.drawLine(startX, startY, startX, endY);
+        g2d.drawLine(startX, startY, startX, endY);         // We draw the axes here
         g2d.drawLine(startX, endY, endX, endY);
         
-        g2d.drawString("10", endX, endY);
+        g2d.drawString("10", endX, endY);               // We draw the labels here
         g2d.drawString("0", startX, endY);
         g2d.drawString(""+max, startX, startY);
 
-        //We draw each of our coords in red color
-        if (gain)
+        
+        if (gain)                                          // gain is positive
             g2d.setColor(Color.GREEN);
         else
             g2d.setColor(Color.RED);
-        // g2d.setColor(Color.RED);
         for (int i = yCoords.size()-10; i < yCoords.size(); i++) {
             if (i < 1)
                 continue;
